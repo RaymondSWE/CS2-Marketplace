512702660ec9e4989203bffa89f9734b64b9ce50
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-07-07 12:32:25 +0200

    added more test

--- src/__tests__/buyCardReducer.test.jsx
+++ src/__tests__/buyCardReducer.test.jsx
@@ -54,3 +54,38 @@ describe('cardReducer', () => {
     expect(cardReducer(state, action)).toEqual(state);
   });
 });
+
+
+it('should handle multiple ADD_TO_CARD actions', () => {
+  const state = {
+    addCard: [],
+  };
+  const action1 = {
+    type: 'ADD_TO_CARD',
+    payload: { assetid: '1', name: 'test' },
+  };
+  const action2 = {
+    type: 'ADD_TO_CARD',
+    payload: { assetid: '2', name: 'test2' },
+  };
+  const expectedState = {
+    addCard: [{ ...action1.payload, qty: 1 }, { ...action2.payload, qty: 1 }],
+  };
+  const newState = cardReducer(state, action1);
+  expect(cardReducer(newState, action2)).toEqual(expectedState);
+});
+
+
+it('should handle REMOVE_FROM_CARD action for non-existing item', () => {
+  const state = {
+    addCard: [{ assetid: '1', name: 'test', qty: 1 }],
+  };
+  const action = {
+    type: 'REMOVE_FROM_CARD',
+    payload: { assetid: '2' },
+  };
+  const expectedState = {
+    addCard: [{ assetid: '1', name: 'test', qty: 1 }],
+  };
+  expect(cardReducer(state, action)).toEqual(expectedState);
+});
