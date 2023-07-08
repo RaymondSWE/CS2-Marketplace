2c9ac3a2010bc4594e4831954a5b3ecf868be7de
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-07-08 23:58:09 +0200

    fixed remove fetch-mock-jest replaced with fetch-mock

--- src/__tests__/SteamAccount.test.jsx
+++ src/__tests__/SteamAccount.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import SteamAccount from "../components/SteamAccount/SteamAccount";
 import "@testing-library/jest-dom";
-import fetchMock from "fetch-mock-jest";
+import fetchMock from "fetch-mock";
 import { toast } from "react-toastify";
 
 jest.mock("react-toastify", () => ({
