de48f99712ba9718213cee85b72aba9bbfe7c327
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-07-08 22:26:48 +0200

    added test to validate trade link correct

--- src/__tests__/SteamAccount.test.jsx
+++ src/__tests__/SteamAccount.test.jsx
@@ -3,6 +3,14 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import SteamAccount from '../components/SteamAccount/SteamAccount';
 import '@testing-library/jest-dom';
 import fetchMock from 'fetch-mock-jest';
+import { toast } from 'react-toastify';
+
+jest.mock('react-toastify', () => ({
+  toast: {
+    error: jest.fn(),
+    success: jest.fn(),
+  },
+}));
 
 describe('SteamAccount', () => {
   
@@ -16,7 +24,6 @@ describe('SteamAccount', () => {
     }
   };
   
-
   beforeEach(() => {
     fetchMock.reset();
   });
@@ -30,6 +37,17 @@ describe('SteamAccount', () => {
     render(<SteamAccount {...mockProps} />);
     // replace getByText with findByText to wait for the element to appear
     expect(await screen.findByText('Test User')).toBeInTheDocument();
+  });
+  it('validates trade link correctly', async () => {
+    render(<SteamAccount {...mockProps} />);
+    const inputField = screen.getByPlaceholderText('Enter your Trade URL here');
+    const applyButton = screen.getByText('Apply');
+    fireEvent.change(inputField, { target: { value: 'Invalid trade link' } });
+    fireEvent.click(applyButton);
+
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith('Invalid Steam trade link', { theme: "colored" });
+    });
   });
   
 });
